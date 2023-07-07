b592560f1bbc48fbbc60b98491b17baada61fbaf
JoaoVitorSFernandes <joaosilvafernandesdev@gmail.com>
2023-07-07 16:02:17 -0300

    Connection String SqLite

--- MiniTodo/Data/AppDbContext.cs
+++ MiniTodo/Data/AppDbContext.cs
@@ -5,5 +5,8 @@ namespace MiniTodo.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
     }
 }
